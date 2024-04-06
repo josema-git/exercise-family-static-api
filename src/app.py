0cce1fc99a6216f23146f4b7be25cb1c47e0b96b
josema-git <152654420+josema-git@users.noreply.github.com>
2024-04-06 05:46:04 +0000

    changes

--- src/app.py
+++ src/app.py
@@ -1,44 +1,45 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
-#from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
-jackson_family = FamilyStructure("Jackson")
+jackson_family = FamilyStructure("jackson",[{"first_name":"John", "age": 33, "lucky_numbers": [7, 13, 22], 'id': 1},{"first_name":"Jane", "age": 35, "lucky_numbers": [10, 14, 3], 'id':2},{"first_name":"Jimmy", "age": 5, "lucky_numbers": [1], 'id': 3}])
 
-# Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json()
+    jackson_family.add_member(body)
+    return jsonify("member added"), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member_route(member_id):
+    jackson_family.delete_member(member_id)
+    return jsonify({"done": True}), 200
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
